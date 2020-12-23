48f83f85c59d94bc3285d92710c1f7ef52a91b2a
Igor <webroma57@gmail.com>
2020-12-24 00:37:36 +0200

    Добавил пакет corss-env, gulp-w3c-html-validator, добавил функции cvh и cvhp для sass, добавил новый клас-модификатор abs_centered

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,18 @@
 var gulp = require('gulp'),
-  browserSync = require('browser-sync').create();
+  browserSync = require('browser-sync').create(),
+  htmlValidator = require('gulp-w3c-html-validator'),
   sass = require('gulp-sass'),
-  spritesmith = require('gulp.spritesmith'),
   notify = require("gulp-notify"),
   autoprefixer = require('autoprefixer'),
   plumber = require('gulp-plumber'),
-  htmlmin = require('gulp-htmlmin'),
-  uglify = require('gulp-uglify'),
   postcss = require('gulp-postcss')
   cssnano = require('cssnano'),
   pug = require('gulp-pug'),
   data = require('gulp-data'),
   fs = require('fs');
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
 
 gulp.task('browser-sync', ['sass', 'pug'], function() {
     browserSync.init({
@@ -23,9 +24,13 @@ gulp.task('browser-sync', ['sass', 'pug'], function() {
 
 gulp.task('sass', function () {
   var plugins = [
-        autoprefixer(),
-        cssnano()
-    ];
+    autoprefixer()
+  ];
+
+  if (isProd) {
+    plugins.push(cssnano());
+  }
+
   return gulp.src('app/sass/**/*.sass')
     .pipe(plumber({
         errorHandler: notify.onError(function(err) {
@@ -57,24 +62,22 @@ gulp.task('pug', function() {
             return JSON.parse(fs.readFileSync('app/templates/data/data.json'));
         }))
       .pipe(pug({
-        "pretty":true /* for desable html minify*/
+        "pretty": isDev /* for desable html minify*/
       }))
       .pipe(gulp.dest('app/'))
       .pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task('sprite', function () {
-  var spriteData = gulp.src('app/img/**/*').pipe(spritesmith({
-    imgName: 'sprite.png',
-    cssName: 'sprite.css'
-  }));
-  return spriteData.pipe(gulp.dest('app/img/sprites/'));
+gulp.task('validateHtml', function() {
+  return gulp.src('app/*.html')
+         .pipe(htmlValidator())
+         .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('default', function () {
   gulp.start('browser-sync', 'sass');
 	gulp.watch(['app/sass/**/*.sass'], ['sass']);
-  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/*.html', ['validateHtml']);
   gulp.watch(['app/templates/**/*.pug'], ['pug']);
 	gulp.watch('app/js/**/*.js', browserSync.reload);
   gulp.watch('app/img/**/*', browserSync.reload);
