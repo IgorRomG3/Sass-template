8ba61ab440fbe20d79e891140dc02fee5804381d
igorromanenko <webroma57@gmail.com>
2022-07-23 15:16:19 +0300

    added sequence function

--- sources/scripts/app/template.js
+++ sources/scripts/app/template.js
@@ -8,4 +8,13 @@
   function initPage() {
     console.log('template');  
   }
+
+  function sequence() {
+    const args = Array.prototype.slice.call(arguments);
+    const funcs = R.type(R.last(args)) == 'Array' ? R.zip([...R.dropLast(1, args)], R.last(args)) : args;
+
+    return () => {
+      R.map(func => R.type(func) == 'Array' ? func[0](R.join(',', R.tail(func))) : func() , funcs);
+    }
+  }
 })();
